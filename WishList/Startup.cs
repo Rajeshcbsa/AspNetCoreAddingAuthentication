cd998539cf319ca37aa6d38a8db8f12ceae156fd
Rajesh <rajeshcbsa@yahoo.co.in>
2018-11-07 01:01:35 +0530

    completed he module

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using WishList.Data;
+using Microsoft.AspNetCore.Identity;
+using WishList.Models;
+using System;
 
 namespace WishList
 {
@@ -12,9 +15,14 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            
+
             services.AddMvc();
             services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("Wishlist"));
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
         }
+
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -28,6 +36,7 @@ namespace WishList
                 app.UseExceptionHandler("/Home/Error");
             }
 
+            app.UseAuthentication();
             app.UseMvcWithDefaultRoute();
         }
     }
